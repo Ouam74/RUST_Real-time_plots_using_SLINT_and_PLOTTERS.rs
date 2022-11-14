dd67e73d7ccd464ce115260b1a004e4c44389be7
Olivier Pelhatre <44462129+Ouam74@users.noreply.github.com>
2022-11-14 16:32:45 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::{thread, time};
+use std::sync::mpsc::{self, channel, sync_channel, Receiver, Sender, TryRecvError};
 use std::sync::{Arc, Mutex};
 use rand::Rng;
 use plotters::prelude::*;
-use slint::{Image, RgbaColor, Color, SharedPixelBuffer, Brush, SharedString};
-
+use slint::{RgbaColor, Color, SharedPixelBuffer, Brush, SharedString};
 
 slint::slint! {
     import { MainWindow } from "src/Thread3.slint";
@@ -20,8 +20,10 @@ fn createserie_iter() -> Vec<(f64, f64)> {
     ret
 }
 
-fn render_plot(newserie: Vec<(f64, f64)>) -> Image {
-    let mut pixel_buffer = SharedPixelBuffer::new(800, 480);
+
+fn render_plot(newserie: Vec<(f64, f64)>) -> slint::Image {
+    let mut pixel_buffer = slint::SharedPixelBuffer::new(800, 480);
+    // let mut pixel_buffer = slint::SharedPixelBuffer::new(1600, 960);
     let size = (pixel_buffer.width(), pixel_buffer.height());
     let backend = plotters::backend::BitMapBackend::with_buffer(pixel_buffer.make_mut_bytes(), size);
 
@@ -39,6 +41,9 @@ fn render_plot(newserie: Vec<(f64, f64)>) -> Image {
 
     chart.configure_mesh().draw().unwrap();
 
+    // chart.draw_series(plotters::series::LineSeries::new(createserie_iter(), &RED).point_size(0))
+    //      .expect("error drawing series");
+
     chart.draw_series(plotters::series::LineSeries::new(newserie, &RED).point_size(0))
     .expect("error drawing series");
 
@@ -46,7 +51,7 @@ fn render_plot(newserie: Vec<(f64, f64)>) -> Image {
     drop(chart);
     drop(root);
 
-    Image::from_rgb8(pixel_buffer)
+    slint::Image::from_rgb8(pixel_buffer)
 }
 
 
@@ -55,81 +60,225 @@ pub fn main() {
     let ui = MainWindow::new();
     let ui_handle = ui.as_weak();
     let abort = Arc::new(Mutex::new(0));
+    let (tx, rx) = mpsc::sync_channel(0);
     
-    let abort_clone = abort.clone(); 
     let ui_handle_clone = ui_handle.clone();
+    let abort_clone = abort.clone(); 
     ui.on_start_clicked({
         move || {
-            *abort.lock().unwrap() = 0;
+            let ui = ui_handle.unwrap();
+            let ui_handle = ui.as_weak();
+            let tx = tx.clone();
 
+            let abort_clone = abort.clone(); 
+            *abort_clone.lock().unwrap() = 0;
+            
             let ui_handle_clone = ui_handle.clone(); 
             let abort_clone = abort.clone(); 
             let thread1 = std::thread::spawn(move || {
                 loop {
                     if *abort_clone.lock().unwrap() == 0 {
                         let newserie = createserie_iter();
-                        // thread::sleep(time::Duration::from_micros(1000));
-                        thread::sleep(time::Duration::from_millis(1));
-                        let handle_copy = ui_handle_clone.clone();
-                        slint::invoke_from_event_loop(move || {
-                            handle_copy.unwrap().set_start_status(true);
-                            handle_copy.unwrap().set_new_image(render_plot(newserie));
-                        });
+                        thread::sleep(time::Duration::from_millis(10));
+                        tx.send(newserie).unwrap();
                     }
                     else {
-                        // println!("abort_clone (thread1) = {}", abort_clone.lock().unwrap());
                         break;
                     };
                 };
-                let handle_copy = ui_handle_clone.clone();
+                let handle_copy = ui_handle.clone();
                 slint::invoke_from_event_loop(move || {
                     handle_copy.unwrap().set_start_status(false);
                 });
             });
-            
-            let ui_handle_clone = ui_handle.clone(); 
+
             let abort_clone = abort.clone(); 
             let thread2 = std::thread::spawn(move || {
                 loop {
                     if *abort_clone.lock().unwrap() == 0 {
-                            let handle_copy = ui_handle_clone.clone();
                             thread::sleep(time::Duration::from_millis(500));
+                            let handle_copy = ui_handle_clone.clone();
                             slint::invoke_from_event_loop(move || {
                                 handle_copy.unwrap().set_current_color(Brush::SolidColor(Color::from(RgbaColor{ red: 0.0, green: 0.0, blue: 1.0, alpha: 1.})));
                                 handle_copy.unwrap().set_current_string(SharedString::from("RUNNING"));
                             });
-                            let handle_copy = ui_handle_clone.clone();
-                            thread::sleep(time::Duration::from_millis(500));
-                            slint::invoke_from_event_loop(move || {
-                                handle_copy.unwrap().set_current_color(Brush::SolidColor(Color::from(RgbaColor{ red: 1.0, green: 0.5, blue: 0.0, alpha: 1.})));
-                                handle_copy.unwrap().set_current_string(SharedString::from("RUNNING"));
-                            });
                     }
+                    if *abort_clone.lock().unwrap() == 0 {
+                        thread::sleep(time::Duration::from_millis(500));
+                        let handle_copy = ui_handle_clone.clone();
+                        slint::invoke_from_event_loop(move || {
+                            handle_copy.unwrap().set_current_color(Brush::SolidColor(Color::from(RgbaColor{ red: 1.0, green: 0.5, blue: 0.0, alpha: 1.})));
+                            handle_copy.unwrap().set_current_string(SharedString::from("RUNNING"));
+                        });
+                    }   
                     else {
-                        // println!("abort_clone (thread2) = {}", abort_clone.lock().unwrap());
+                        let handle_copy = ui_handle_clone.clone();
+                        slint::invoke_from_event_loop(move || {
+                            handle_copy.unwrap().set_current_color(Brush::SolidColor(Color::from(RgbaColor{ red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0})));
+                            handle_copy.unwrap().set_current_string(SharedString::from("       "));
+                        });
                         break;
                     };
                 }
-                let handle_copy = ui_handle_clone.clone();
-                slint::invoke_from_event_loop(move || {
-                    handle_copy.unwrap().set_current_color(Brush::SolidColor(Color::from(RgbaColor{ red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0})));
-                    handle_copy.unwrap().set_current_string(SharedString::from("       "));
-                });
             });
-
-            // thread1.join().unwrap();
-            // thread2.join().unwrap();
-
         }
+    });
+    
+    let ui_handle = ui.as_weak();
+    std::thread::spawn(move || {
+        for received in rx {
+            let handle_copy = ui_handle.clone();
+            slint::invoke_from_event_loop(move || {
+                handle_copy.unwrap().set_new_image(render_plot(received));
+            });
+        };
     });
     
     let abort_clone = abort_clone.clone(); // let abort_clone = Arc::clone(&abort_clone);
     ui.on_stop_clicked({
         move || {
             *abort_clone.lock().unwrap() = 1;
-            // println!("abort_clone = {}", abort_clone.lock().unwrap());
         }
     });
     
     ui.run();
 }
+
+
+// use std::{thread, time};
+// use std::sync::{Arc, Mutex};
+// use rand::Rng;
+// use plotters::prelude::*;
+// use slint::{Image, RgbaColor, Color, SharedPixelBuffer, Brush, SharedString};
+
+
+// slint::slint! {
+//     import { MainWindow } from "src/Thread3.slint";
+// }
+
+// static _N: i64 = 1000;
+
+// fn createserie_iter() -> Vec<(f64, f64)> {
+//     let mut ret = vec![];
+//     let mut _rng = rand::thread_rng();
+//     for j in 0.._N {
+//         ret.push( ( j as f64, _rng.gen::<f64>() ) );
+//     }
+//     ret
+// }
+
+// fn render_plot(newserie: Vec<(f64, f64)>) -> Image {
+//     let mut pixel_buffer = SharedPixelBuffer::new(800, 480);
+//     let size = (pixel_buffer.width(), pixel_buffer.height());
+//     let backend = plotters::backend::BitMapBackend::with_buffer(pixel_buffer.make_mut_bytes(), size);
+
+//     // Plotters requires TrueType fonts from the file system to draw axis text - we skip that for WASM for now. 
+//     #[cfg(target_arch = "wasm32")]
+//     let backend = slint::wasm_backend::BackendWithoutText { backend };
+    
+//     let root = backend.into_drawing_area();
+
+//     root.fill(&WHITE).expect("error filling drawing area");
+
+//     let mut chart = plotters::chart::ChartBuilder::on(&root)
+//         .build_cartesian_2d(0.0.._N as f64, 0.0..1.0)
+//         .unwrap();
+
+//     chart.configure_mesh().draw().unwrap();
+
+//     chart.draw_series(plotters::series::LineSeries::new(newserie, &RED).point_size(0))
+//     .expect("error drawing series");
+
+//     root.present().expect("error presenting");
+//     drop(chart);
+//     drop(root);
+
+//     Image::from_rgb8(pixel_buffer)
+// }
+
+
+// pub fn main() {
+    
+//     let ui = MainWindow::new();
+//     let ui_handle = ui.as_weak();
+//     let abort = Arc::new(Mutex::new(0));
+    
+//     let ui_handle_clone = ui_handle.clone();
+//     let abort_clone = abort.clone(); 
+//     ui.on_start_clicked({
+//         move || {
+//             *abort.lock().unwrap() = 0;
+
+//             let ui_handle_clone = ui_handle.clone(); 
+//             let abort_clone = abort.clone(); 
+//             let thread1 = std::thread::spawn(move || {
+//                 loop {
+//                     let newserie = createserie_iter();
+//                     thread::sleep(time::Duration::from_millis(1));
+//                     if *abort_clone.lock().unwrap() == 0 {
+//                         let handle_copy = ui_handle_clone.clone();
+//                         slint::invoke_from_event_loop(move || {
+//                             handle_copy.unwrap().set_start_status(true);
+//                             handle_copy.unwrap().set_new_image(render_plot(newserie));
+//                         });
+//                     }
+//                     else {
+//                         // println!("abort_clone (thread1) = {}", abort_clone.lock().unwrap());
+//                         break;
+//                     };
+//                 };
+//                 let handle_copy = ui_handle_clone.clone();
+//                 slint::invoke_from_event_loop(move || {
+//                     handle_copy.unwrap().set_start_status(false);
+//                 });
+//             });
+            
+//             let ui_handle_clone = ui_handle.clone();
+//             let abort_clone = abort.clone();  
+//             let thread2 = std::thread::spawn(move || {
+//                 loop {
+//                     if *abort_clone.lock().unwrap() == 0 {
+//                             let handle_copy = ui_handle_clone.clone();
+//                             thread::sleep(time::Duration::from_millis(500));
+//                             slint::invoke_from_event_loop(move || {
+//                                 handle_copy.unwrap().set_current_color(Brush::SolidColor(Color::from(RgbaColor{ red: 0.0, green: 0.0, blue: 1.0, alpha: 1.})));
+//                                 handle_copy.unwrap().set_current_string(SharedString::from("RUNNING"));
+//                             });
+//                     }
+//                     if *abort_clone.lock().unwrap() == 0 {
+//                         let handle_copy = ui_handle_clone.clone();
+//                         thread::sleep(time::Duration::from_millis(500));
+//                         slint::invoke_from_event_loop(move || {
+//                             handle_copy.unwrap().set_current_color(Brush::SolidColor(Color::from(RgbaColor{ red: 1.0, green: 0.5, blue: 0.0, alpha: 1.})));
+//                             handle_copy.unwrap().set_current_string(SharedString::from("RUNNING"));
+//                         });
+//                     }   
+//                     else {
+//                         // println!("abort_clone (thread2) = {}", abort_clone.lock().unwrap());
+//                         break;
+//                     };
+//                 }
+//                 let handle_copy = ui_handle_clone.clone();
+//                 slint::invoke_from_event_loop(move || {
+//                     handle_copy.unwrap().set_current_color(Brush::SolidColor(Color::from(RgbaColor{ red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0})));
+//                     handle_copy.unwrap().set_current_string(SharedString::from("       "));
+//                 });
+//             });
+
+//             // thread1.join().unwrap();
+//             // thread2.join().unwrap();
+
+//         }
+//     });
+    
+//     let abort_clone = abort_clone.clone(); // let abort_clone = Arc::clone(&abort_clone);
+//     ui.on_stop_clicked({
+//         move || {
+//             *abort_clone.lock().unwrap() = 1;
+//             // println!("abort_clone = {}", abort_clone.lock().unwrap());
+//         }
+//     });
+    
+//     ui.run();
+// }
+
